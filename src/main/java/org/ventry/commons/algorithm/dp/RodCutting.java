a0e6ab89b459060e682127d3e9d988bce688d384
ventry <ventry1990@gmail.com>
2017-08-15 21:21:01 +0800

    dp -> rod cutting

--- src/main/java/org/ventry/commons/algorithm/dp/RodCutting.java
+++ src/main/java/org/ventry/commons/algorithm/dp/RodCutting.java
@@ -11,28 +11,46 @@ import org.ventry.commons.utils.Console;
 
 public class RodCutting {
     private int[] prices;
+    private int[] solution;
 
-    private RodCutting(int[] p) {
-        this.prices = p;
+    private RodCutting(int[] prices) {
+        this.prices = prices;
     }
 
     private int cut(int length) {
         int[] result = new int[length + 1];
+        solution = new int[length + 1];
         result[0] = 0;
 
         for (int i = 1; i <= length; i++) {
-            int temp = Integer.MIN_VALUE;
+            int maxProfit = Integer.MIN_VALUE;
             for (int j = 1; j <= i; j++) {
-                temp = Math.max(temp, prices[j - 1] + result[i - j]);
+                int curProfit = prices[j - 1] + result[i - j];
+                if (maxProfit < curProfit) {
+                    maxProfit = curProfit;
+                    solution[i] = j;
+                }
             }
-            result[i] = temp;
+            result[i] = maxProfit;
         }
+
+        printSolution(length);
         return result[length];
     }
 
+    private void printSolution(int length) {
+        while (length > 0) {
+            System.out.print(solution[length] + " | ");
+            length = length - solution[length];
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        RodCutting rodCutting = new RodCutting(new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 30});
-        for (int i = 0; i < 11; i++) {
+        int[] p = new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        RodCutting rodCutting = new RodCutting(p);
+        for (int i = 0; i <= p.length; i++) {
+            Console.writeLine("\n>> " + i + ": ");
             Console.writeLine(rodCutting.cut(i));
         }
     }
