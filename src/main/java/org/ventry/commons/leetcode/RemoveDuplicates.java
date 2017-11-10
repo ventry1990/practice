7006ef95ab493436abf1dbb2f5e797f39d1f2a1f
ventry <ventry1990@gmail.com>
2017-11-10 20:07:01 +0800

    leetcode -> remove duplicates

---
+++ src/main/java/org/ventry/commons/leetcode/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package org.ventry.commons.leetcode;
+
+/**
+ * file: org.ventry.commons.leetcode.RemoveDuplicates
+ * author: ventry
+ * create: 17/11/10 19:48
+ * description:
+ */
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums == null)
+            return 0;
+        if (nums.length < 2)
+            return nums.length;
+
+        int dup = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                dup++;
+            } else {
+                nums[i - dup] = nums[i];
+            }
+        }
+
+        return nums.length - dup;
+    }
+}
