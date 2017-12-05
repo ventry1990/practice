5c854595619b0c748f8968b4679b198a6786b7e5
ventry <ventry1990@gmail.com>
2017-12-05 12:23:24 +0800

    leetcode -> jump game 2

--- src/main/java/org/ventry/commons/leetcode/JumpGame.java
+++ src/main/java/org/ventry/commons/leetcode/JumpGame.java
@@ -22,4 +22,43 @@ public class JumpGame {
 
         return e >= nums.length;
     }
+
+    public int jump(int[] nums) {
+        if (nums.length < 2) return 0;
+
+        int s = 0;
+        int e = s + nums[s] + 1;
+        int max = e;
+        int step = 1;
+        for (int i = s; i < e && e < nums.length; i++) {
+            if (nums[i] + i + 1 > max) {
+                max = nums[i] + i + 1;
+            }
+
+            if (i == e - 1 && max > e) {
+                e = max;
+                step++;
+            }
+        }
+
+        return e >= nums.length ? step : 0;
+    }
+
+    public int jump2(int[] nums) {
+        if (nums.length < 2) return 0;
+
+        int curMax = nums[0];
+        int nextMax = nums[0];
+        int curStep = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (i > curMax) {
+                curMax = nextMax;
+                curStep++;
+            }
+
+            if (i + nums[i] > nextMax)
+                nextMax = i + nums[i];
+        }
+        return curStep;
+    }
 }
