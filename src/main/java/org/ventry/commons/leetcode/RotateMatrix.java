2a5cd2cc100e025bdf751d3d214e9f35efdaa7eb
ventry <ventry1990@gmail.com>
2017-12-13 17:14:49 +0800

    leetcode -> rotate matrix

---
+++ src/main/java/org/ventry/commons/leetcode/RotateMatrix.java
@@ -0,0 +1,35 @@
+package org.ventry.commons.leetcode;
+
+/**
+ * file: org.ventry.commons.leetcode.RotateMatrix
+ * author: ventry
+ * create: 17/12/8 17:40
+ * description:
+ */
+
+public class RotateMatrix {
+
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        for (int i = 0; i < len / 2; i++) {
+            int end = len - 1 - i;
+            for (int j = i; j < end; j++) {
+                swap(matrix, i, j);
+            }
+        }
+    }
+
+    private static void swap(int[][] matrix, int x, int y) {
+        int newX = x;
+        int newY = y;
+        int tmp;
+        for (int i = 0; i < 4; i++) {
+            int t = matrix.length - 1 - newX;
+            newX = newY;
+            newY = t;
+            tmp = matrix[newX][newY];
+            matrix[newX][newY] = matrix[x][y];
+            matrix[x][y] = tmp;
+        }
+    }
+}
