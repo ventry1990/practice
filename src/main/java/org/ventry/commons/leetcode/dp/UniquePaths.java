f2938b67cf17dda71e6bd94ed86ee26689d7f43a
ventry <ventry1990@gmail.com>
2018-07-19 16:22:05 +0800

    leetcode -> unique paths II

--- src/main/java/org/ventry/commons/leetcode/dp/UniquePaths.java
+++ src/main/java/org/ventry/commons/leetcode/dp/UniquePaths.java
@@ -30,4 +30,26 @@ public class UniquePaths {
 
         return cur[m - 1];
     }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m, n;
+        if ((m = obstacleGrid.length) == 0 || (n = obstacleGrid[0].length) == 0) {
+            return 0;
+        }
+
+        int[][] paths = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            paths[i][0] = 1;
+        }
+        for (int i = 0; i < n && obstacleGrid[0][i] == 0; i++) {
+            paths[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                paths[i][j] = obstacleGrid[i][j] == 1 ? 0 : paths[i - 1][j] + paths[i][j - 1];
+            }
+        }
+        return paths[m - 1][n - 1];
+    }
 }
