d50df1ca5f3e60f21f24d292a806d4d476193093
ventry <ventry1990@gmail.com>
2018-12-17 17:52:26 +0800

    leetcode -> combinations

---
+++ src/main/java/org/ventry/commons/leetcode/backtracking/Combinations.java
@@ -0,0 +1,41 @@
+package org.ventry.commons.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * file: org.ventry.commons.leetcode.backtracking.Combinations
+ * author: ventry
+ * create: 2018/12/17 16:49
+ * description:
+ */
+
+public class Combinations {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (n <= 0 || k <= 0)
+            return res;
+
+        int[] one = new int[k + 1];
+        backtracking(res, one, n, 1, k);
+        return res;
+    }
+
+    private void backtracking(List<List<Integer>> res, int[] one,
+                              int n, int i, int k) {
+        if (i == k + 1) {
+            List<Integer> ans = new ArrayList<>(k);
+            for (int j = 1; j < one.length; j++) {
+                ans.add(one[j]);
+            }
+            res.add(ans);
+            return;
+        }
+
+        for (int j = one[i - 1] + 1; j <= n; j++) {
+            one[i] = j;
+            backtracking(res, one, n, i + 1, k);
+        }
+    }
+}
--- src/main/java/org/ventry/commons/leetcode/dp/MinimumPathSum.java
+++ src/main/java/org/ventry/commons/leetcode/dp/MinimumPathSum.java
@@ -30,4 +30,19 @@ public class MinimumPathSum {
 
         return dp[m - 1][n - 1];
     }
+
+    public int minPathSum2(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = (i > 0 ? dp[i - 1] : 0) + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
 }
