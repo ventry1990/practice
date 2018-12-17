3034e487b61a1f8050b2967b4eb616e55562f12c
ventry <ventry1990@gmail.com>
2018-12-17 18:40:27 +0800

    add greatest common divisor

---
+++ src/main/java/org/ventry/commons/utils/MathUtils.java
@@ -0,0 +1,26 @@
+package org.ventry.commons.utils;
+
+/**
+ * file: org.ventry.commons.utils.MathUtils
+ * author: ventry
+ * create: 2018/12/17 18:35
+ * description:
+ */
+
+public class MathUtils {
+
+    public static int gcd(int a, int b) {
+        if (a > b) {
+            int t = b;
+            b = a;
+            a = t;
+        }
+
+        while (a > 0) {
+            int r = b % a;
+            b = a;
+            a = r;
+        }
+        return b;
+    }
+}
