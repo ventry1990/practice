fe36f5ddc22547799c515cb8e3c98fd6bb719ec8
ventry <ventry1990@gmail.com>
2019-03-16 14:54:36 +0800

    leetcode -> path sum II

--- src/main/java/org/ventry/commons/leetcode/tree/PathSum.java
+++ src/main/java/org/ventry/commons/leetcode/tree/PathSum.java
@@ -2,6 +2,9 @@ package org.ventry.commons.leetcode.tree;
 
 import org.ventry.commons.leetcode.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * file: org.ventry.commons.leetcode.tree.PathSum
  * author: ventry
@@ -19,6 +22,30 @@ public class PathSum {
         } else {
             sum = sum - root.val;
             return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+        }
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        find(root, sum, res, path);
+        return res;
+    }
+
+    private void find(TreeNode root, int sum, List<List<Integer>> res, List<Integer> path) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                List<Integer> one = new ArrayList<>(path);
+                one.add(sum);
+                res.add(one);
+            }
+        } else {
+            sum = sum - root.val;
+            path.add(root.val);
+            find(root.left, sum, res, path);
+            find(root.right, sum, res, path);
+            path.remove(path.size() - 1);
         }
     }
 }
