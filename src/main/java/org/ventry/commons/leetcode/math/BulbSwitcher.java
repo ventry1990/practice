16b336cfcd36468535edaef6d6f108a24acbe8ca
ventry <ventry1990@gmail.com>
2020-03-24 15:53:33 +0800

    leetcode -> bulb switcher

---
+++ src/main/java/org/ventry/commons/leetcode/math/BulbSwitcher.java
@@ -0,0 +1,14 @@
+package org.ventry.commons.leetcode.math;
+
+/**
+ * file: org.ventry.commons.leetcode.math.BulbSwitcher
+ * author: ventry
+ * create: 2020/3/24 15:15
+ * description:
+ */
+public class BulbSwitcher {
+
+    public int bulbSwitch(int n) {
+        return (int) Math.pow(n, 0.5);
+    }
+}
